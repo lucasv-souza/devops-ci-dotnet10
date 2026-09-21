4699e4595b30161edeb160fb873741229eb08497
Lucas V.S <lucas.viniciuss@outlook.com>
2026-09-21 08:55:25 -0300

    test: demonstra deteccao de regressao pelo CI

--- src/DevOps.Api/Services/CalculadoraService.cs
+++ src/DevOps.Api/Services/CalculadoraService.cs
@@ -1,10 +1,10 @@
-﻿namespace DevOps.Api.Services;
+namespace DevOps.Api.Services;
 
 public class CalculadoraService
 {
     public int Somar(int primeiroNumero, int segundoNumero)
     {
-        return primeiroNumero + segundoNumero;
+        return primeiroNumero - segundoNumero;
     }
 
     public bool EhPar(int numero)
